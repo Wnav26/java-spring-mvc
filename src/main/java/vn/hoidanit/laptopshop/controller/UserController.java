37583441c84c2e94c874dd0fed44667c97ed9d38
Wnav26 <nvan0330@gmail.com>
2024-10-24 15:06:37 +0700

    to chuc lai mohinh mvc bai 45

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,23 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// @Controller
+// public class UserController {
+//     @RequestMapping("/")
+//     public String getHomePage() {
+//         return "hello controller";
+//     }
+
+// }
+
+@RestController
+public class UserController {
+    @GetMapping("/")
+    public String getHomePage() {
+        return "hello controller";
+    }
+
+}
