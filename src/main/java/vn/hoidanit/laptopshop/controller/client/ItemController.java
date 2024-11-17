82e3279fa43cdde8b7cc37c59f7daede67ccfcaa
Wnav26 <nvan0330@gmail.com>
2024-11-17 17:00:32 +0700

    hoan thanh trang home va detail san pham client

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -5,10 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.service.ProductService;
+
 @Controller
 public class ItemController {
+    private final ProductService productService;
+
+    public ItemController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @RequestMapping("/product/{id}")
     public String getProductDetailPage(Model model, @PathVariable long id) {
+        Product products = this.productService.getProductById(id).get();
+        model.addAttribute("id", id);
+        model.addAttribute("product", products);
         return "client/product/detail";
     }
 }
